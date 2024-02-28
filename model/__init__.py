dd8746f9a7b2673859a06fe8aa5fd440b6f42e95
hanzh0816 <15091693112@163.com>
2024-02-28 19:21:53 +0800

    Changes to be committed: 	modified:   model/__init__.py 	new file:   model/spikeformer_origin.py 	modified:   module/__init__.py 	new file:   module/origin/__init__.py 	new file:   module/origin/ms_conv.py 	new file:   module/origin/sps.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,9 @@
-from pyexpat import model
+import torch.nn as nn
 from .spikeformer import SpikeDrivenTransformer
 from .resnet import ResNet50
+
+from .spikeformer_origin import SpikeDrivenTransformer as sdt_origin
+from timm.models.vision_transformer import _cfg
 
 
 def build_model(config):
@@ -23,6 +26,32 @@ def build_model(config):
         )
     elif config.MODEL.NAME.startswith("ResNet"):
         model = ResNet50(config.MODEL.NUM_CLASSES)
+    elif config.MODEL.NAME == "sdt-origin":
+        model = sdt_origin(
+            img_size_h=config.DATA.IMG_SIZE,
+            img_size_w=config.DATA.IMG_SIZE,
+            patch_size=config.MODEL.PATCH_SIZE,
+            in_channels=config.DATA.CHANNELS,
+            num_classes=config.MODEL.NUM_CLASSES,
+            embed_dims=config.MODEL.EMBED_DIMS,
+            num_heads=config.MODEL.NUM_HEADS,
+            mlp_ratios=config.MODEL.MLP_RATIO,
+            qkv_bias=False,
+            qk_scale=None,
+            drop_rate=0.0,
+            attn_drop_rate=0.0,
+            drop_path_rate=0.0,
+            norm_layer=nn.LayerNorm,
+            depths=config.MODEL.NUM_LAYERS,
+            sr_ratios=1,
+            T=config.MODEL.SPIKE_T,
+            pooling_stat=config.MODEL.POOL_STAT,
+            attn_mode="direct_xor",
+            spike_mode="lif",
+            dvs_mode=False,
+            TET=False,
+        )
+        model.default_cfg = _cfg()
     else:
         raise NotImplementedError
     return model
--- module/__init__.py
+++ module/__init__.py
@@ -2,4 +2,7 @@ from .SPS import MS_SPS
 from .Encoder import Encoder
 from .Decoder import Decoder
 
-__all__ = ["MS_SPS", "Encoder", "Decoder"]
+from .origin.ms_conv import MS_Block_Conv
+from .origin.sps import MS_SPS as MS_SPS_Origin
+
+__all__ = ["MS_SPS", "Encoder", "Decoder", "MS_Block_Conv", "MS_SPS_Origin"]
